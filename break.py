107e9370a55ce2f2b7accef785c917c356ea2d64
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-06 16:56:26 +0530

    break

---
+++ break.py
@@ -0,0 +1,16 @@
+i = 1
+while(i<=10):
+    if((i % 6)==0):
+        break
+    print(i)
+    i+=1
+print("outside loop now")
+
+
+n = int(input("enter the number"))
+while(n<=10):
+    if(n%7==0):
+        break
+    print(n)
+    n+=1
+print("outside the loop now:")
