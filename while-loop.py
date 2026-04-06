68abae68439a6fd0503e5e21f01488f73eebc0a5
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-06 14:17:52 +0530

    while loop

---
+++ while-loop.py
@@ -0,0 +1,33 @@
+#infinite loop
+ 
+while True:
+    print("hello world")
+
+
+#while 
+
+count = 1 #iterator
+while(count<=10):
+    print("hello world",count)
+    count +=1
+
+print("after loop count value:", count)
+
+count = 1
+while(count<=5):
+    print(count)
+    count+=1
+print("after loop break count value :",count)
+
+
+count = 5
+while(count>=1):
+    print(count)
+    count-=1
+
+
+count = 10
+while(count>=1):
+    print(count)
+    count-=1
+
