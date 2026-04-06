a5e2f2bffb2748c0e905056affbe7ba1f1b0e4de
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-06 14:13:10 +0530

    match case

---
+++ match-case.py
@@ -0,0 +1,12 @@
+color = (input("enter color:"))
+match color:
+    case "green":
+        print("go")
+    case "yellow":
+        print("look")
+    case "red":
+        print("stop")
+    case __:
+        print("wrong color")
+
+color = (input("enter color:"))
