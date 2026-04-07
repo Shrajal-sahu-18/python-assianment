86d00d8baccaa9df51d9c78661aefd6301f295a6
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-07 15:48:19 +0530

    added factorial code

--- function.py
+++ function.py
@@ -36,3 +36,12 @@ print(sum(100,456))
 
 avg = lambda a,b,c: (a+b+c)/3
 print(avg(2,2,2))
+
+
+def factorial(n):
+    fact = 1
+    for i in range (1,n+1):
+        fact*=i
+    return fact
+n= int(input("enter number:"))
+print(factorial(n))
