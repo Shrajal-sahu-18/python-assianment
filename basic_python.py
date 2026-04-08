9e6bb8e23175c27b7ecd1bbed68abc50163ab014
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-08 18:19:20 +0530

    data type

---
+++ basic_python.py
@@ -0,0 +1,15 @@
+marks = [14,12,34,34,23,3,23]
+
+print(type(marks))
+print(marks[4])
+marks[2] = 40
+print(marks)
+print(marks[1:])
+
+#i.append
+
+list = [1,2,3,4,5,6,0]
+list.append(8)
+print(list)
+list.insert(2,10)
+print(list)
