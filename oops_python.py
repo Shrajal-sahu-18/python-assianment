a8bdc1f4717e316c3f6bcd178bb1eb8f3058219f
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-10 16:14:51 +0530

    oops in python

---
+++ oops_python.py
@@ -0,0 +1,19 @@
+class student:
+    subject = "python"
+    collage = "abc collage"
+    year = "4th year"
+    
+a = 10
+stud1 = student()
+stud2 = student()
+print(stud1.subject,stud1.collage)
+print(stud2.subject,stud2.collage)
+print(type(stud1))
+l = [1,2]
+print(type(l))
+s = set()
+print(type(s))
+tup = (1,2),
+print(type(tup))
+def fun():
+    print("/.....")
