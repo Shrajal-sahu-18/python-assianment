d3ab4701380562b75dd314d7ca6e65aa42d3431e
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-13 23:11:29 +0530

    exception handling

--- file_IO.py
+++ file_IO.py
@@ -1,11 +1,25 @@
-data = True
-word = "python"
-line = 1
-with open("sample.txt","r") as f:
-    while data:
-        data = f.readline()
-        if word in data:
-            print(f"{word} find in line number {line}")
-            break
-        print(data)
-        line+=1
+# data = True
+# word = "python"
+# line = 1
+# with open("sample.txt","r") as f:
+#     while data:
+#         data = f.readline()
+#         if word in data:
+#             print(f"{word} find in line number {line}")
+#             break
+#         print(data)
+#         line+=1
+
+
+#exeption handlin
+try:
+
+    x = int(input("enter x:"))
+
+    ans = 10/x
+except ZeroDivisionError:
+    print(f"dividing by zero is not allowed")
+
+else:
+
+    print(f"ans = {ans}")
