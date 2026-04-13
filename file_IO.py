7cb67f7f8dabbb00f15485de972da4aeaf194513
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-13 23:20:41 +0530

    exception handling

--- file_IO.py
+++ file_IO.py
@@ -1,17 +1,17 @@
-# data = True
-# word = "python"
-# line = 1
-# with open("sample.txt","r") as f:
-#     while data:
-#         data = f.readline()
-#         if word in data:
-#             print(f"{word} find in line number {line}")
-#             break
-#         print(data)
-#         line+=1
+data = True
+word = "python"
+line = 1
+with open("sample.txt","r") as f:
+    while data:
+        data = f.readline()
+        if word in data:
+            print(f"{word} find in line number {line}")
+            break
+        print(data)
+        line+=1
 
 
-#exeption handlin
+#exeption handling
 try:
 
     x = int(input("enter x:"))
@@ -34,3 +34,6 @@ except ValueError:
 
 else:
     print("f ans = {ans}")
+
+finally:
+    print("end of program")
