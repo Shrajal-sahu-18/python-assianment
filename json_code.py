e311c7a4e16538eb6a5aeb23ac945f1b206ae0c2
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-18 14:35:25 +0530

    assianment problem

--- json_code.py
+++ json_code.py
@@ -6,3 +6,10 @@ with open("sample.txt","r") as f:
     print("\n names in files")
     for line in f:
         print(line.strip())
+
+with open("sample.txt","a") as f:
+    data = f.write("program run successfull\n")
+with open("sample.txt","r") as f:
+    data = f.read()
+    print("logs:\n")
+    print(data)
