25afaafb627cf0f26ed0d516264f2af3318178dd
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-18 14:24:16 +0530

    assainment problem

---
+++ json_code.py
@@ -0,0 +1,8 @@
+with open("sample.txt","w") as f:
+    for i in range(5):
+        name = input(f"enter name{i+1}")
+        f.write(name +"\n")
+with open("sample.txt","r") as f:
+    print("\n names in files")
+    for line in f:
+        print(line.strip())
