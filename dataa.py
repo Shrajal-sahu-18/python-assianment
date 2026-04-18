82dd9de264bf7c062a51cdcbecbfca24bc6d1f09
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-18 15:54:56 +0530

    exception handing

--- dataa.py
+++ dataa.py
@@ -23,3 +23,36 @@ with open("cities.json","w") as f:
 print("data updated successfully!")
 print(data)
 
+import json
+cities = {
+    "delhi":10000000,
+    "banglore":2000000,
+    "mumbai":30000000
+}
+with open("cities.json","w") as f:
+    json.dump(cities,f)
+with open("cities.json","r") as f:
+    data = json.load(f)
+print("cities and their population")
+for city,pop in data.items():
+    print(city,":",pop)
+
+new_city = input("enter city name:")
+new_population = int(input("enter city population:"))
+data[new_city] = new_population
+with open("cities.json","w") as f:
+    json.dump(data,f)
+print("data updated successfully!")
+print(data)
+
+
+
+#new question
+try:
+    with open("data.txt","r") as f:
+        data = f.read()
+    print(data)
+except FileNotFoundError:
+    print("file not found")
+finally:
+    print("program end successfully!")
