5c09eb3612ee54abde7f6e340f2b8a2b5c5cfc45
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-19 17:37:54 +0530

    numpy

--- numpy/num_py.py
+++ numpy/num_py.py
@@ -14,3 +14,8 @@ start = time.time()
 sqare = np_arr ** 2
 end = time.time()
 print(f"numpy array time = {end-start}")
+
+
+import sys
+print(f"size of py_list = {sys.getsizeof(py_list)*len(py_list)}bytes")
+print(f"size of numpy array = {np_arr.nbytes} bytes")
