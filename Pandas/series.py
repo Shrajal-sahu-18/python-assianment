12de208dc1faf582425ff5c14a43556a3bc1bad0
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-23 22:05:12 +0530

    label based access

--- Pandas/series.py
+++ Pandas/series.py
@@ -1,3 +1,10 @@
 import pandas as pd
 s = pd.Series([1,2,3,4,5])
 print(s)
+
+#label based access
+s = pd.Series([24,25,26,27],index = ["shrajal","shakti","aditya","rishav"] )
+print(s)
+print(s["shrajal"])
+print(s["rishav"])
+print(s.index)
