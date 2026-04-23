b994e1ca0c27c409949a66cbcaec82b1502f49f8
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-23 22:07:16 +0530

    index based access

--- Pandas/series.py
+++ Pandas/series.py
@@ -8,3 +8,13 @@ print(s)
 print(s["shrajal"])
 print(s["rishav"])
 print(s.index)
+
+#index based access
+s1 = pd.Series([1,2,3,4,5])
+s2 = pd.Series([1,2,3,4,5])
+print(s1+s2)
+s1[1] = 100
+print(s1)
+changed_s1 = s1.drop(1)
+print(s1)
+print(changed_s1)
