5551e806e24218553d1681e9362fe1f682ff7133
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-24 23:04:33 +0530

    head fnx

--- Pandas/data.py
+++ Pandas/data.py
@@ -3,5 +3,8 @@ import pandas as pd
 df = pd.read_csv("raw_data.csv")
 print(df)
 
-df = pd.read_json("empolyee_data.json")
-print(df)
+# df = pd.read_json("empolyee_data.json")
+# print(df)
+
+#methods in pandas
+print(df.head())
