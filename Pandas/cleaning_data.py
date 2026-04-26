51c7292465d4580f26649a54f534b3abe20af5dd
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-26 22:24:03 +0530

    iloc fnx

--- Pandas/cleaning_data.py
+++ Pandas/cleaning_data.py
@@ -5,5 +5,8 @@ print(df["aqi"])
 print(df[["country","aqi"]])
 
 #row
-print(df.loc[0:1])
+print(df.loc[0:1]) # slicing starting index : included last index
 print(df.loc[0:2,["country","city","latitude","aqi"]])
+
+print(df.iloc[0:3,1:5]) #slicing starting index : ending index not included
+print(df.iloc[0:2,1:3])
