6b4535ff4b975969709a3235b387eab9034e894a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-26 23:43:11 +0530

    filtiring data

--- Pandas/data.py
+++ Pandas/data.py
@@ -1,56 +1,56 @@
 import pandas as pd
 
 df = pd.read_csv("raw_data.csv")
-# print(df)
+print(df)
 
-# # df = pd.read_json("empolyee_data.json")
-# # print(df)
+df = pd.read_json("empolyee_data.json")
+print(df)
 
 # #methods in pandas
-# print(df.head())  #first five row
-# print(df.tail())  #last five rows
-# print(df.sample(5)) #for random value
-# print(df.info()) #for information
-# print(df.shape) #row and column
-# print(df.describe) #for details
-# print(df.columns) #for columns
-# print(df.nunique()) #for unique value
+print(df.head())  #first five row
+print(df.tail())  #last five rows
+print(df.sample(5)) #for random value
+print(df.info()) #for information
+print(df.shape) #row and column
+print(df.describe) #for details
+print(df.columns) #for columns
+print(df.nunique()) #for unique value
 
-# df.isnull()
-# df.isna()
-# df.isnull().sum()
-# df.dropna() #empty row drop
-# df.dropna(axis=1)
-# df.fillna(0)
+df.isnull()
+df.isna()
+df.isnull().sum()
+df.dropna() #empty row drop
+df.dropna(axis=1)
+df.fillna(0)
 
 # # # practice problem
-# age_mean = df["age"].mean()
-# cleaned_data = df.copy()
-# cleaned_data["age"] = cleaned_data["age"].fillna(age_mean)
+age_mean = df["age"].mean()
+cleaned_data = df.copy()
+cleaned_data["age"] = cleaned_data["age"].fillna(age_mean)
 
 
 # #type converstion
-# df = pd.read_csv("raw_data.csv") 
-# print(df)
-# print(df.dtypes)
-# df2 = df.copy()
-# df2 = df2.fillna(0)
-# df2["age"]  = df2["age"].astype("int64")
-# print(df2,df2.dtypes)
+df = pd.read_csv("raw_data.csv") 
+print(df)
+print(df.dtypes)
+df2 = df.copy()
+df2 = df2.fillna(0)
+df2["age"]  = df2["age"].astype("int64")
+print(df2,df2.dtypes)
 
-# print(df["gender"].str.lower())
-# print(df["gender"].str.upper())
-# print(df["gender"].str.capitalize())
-# print(df["name"].str.split(" "))
-# print(df["country"].str.contains("US"))
-# print(df["country"].str.contains("india", case=False))
+print(df["gender"].str.lower())
+print(df["gender"].str.upper())
+print(df["gender"].str.capitalize())
+print(df["name"].str.split(" "))
+print(df["country"].str.contains("US"))
+print(df["country"].str.contains("india", case=False))
 
-# df = pd.read_csv("globalAirQuality.csv")
-# df["timestamp"] = pd.to_datetime(df["timestamp"])
-# print(df["timestamp"].dtypes)
+df = pd.read_csv("globalAirQuality.csv")
+df["timestamp"] = pd.to_datetime(df["timestamp"])
+print(df["timestamp"].dtypes)
 
-# date_str = pd.Series([pd.to_datetime("2026-12-31")])
-# print(type(date_str.dtypes))
+date_str = pd.Series([pd.to_datetime("2026-12-31")])
+print(type(date_str.dtypes))
 
 print(df)
 df2 = df.copy()
