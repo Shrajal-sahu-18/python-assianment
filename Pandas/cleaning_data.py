5653bf2d843b5ae3515367276c8026a4ddc3415d
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-26 22:18:53 +0530

    loc fnx

--- Pandas/cleaning_data.py
+++ Pandas/cleaning_data.py
@@ -3,3 +3,7 @@ df = pd.read_csv("globalAirQuality.csv")
 print(df["country"])
 print(df["aqi"])
 print(df[["country","aqi"]])
+
+#row
+print(df.loc[0:1])
+print(df.loc[0:2,["country","city","latitude","aqi"]])
