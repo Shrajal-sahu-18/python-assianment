16c8f21951d661d9c2d13e6699feed1b263326bc
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-27 17:22:57 +0530

    rename fnx for row

--- Pandas/data.py
+++ Pandas/data.py
@@ -63,4 +63,4 @@ df2 = df2.assign(new_income = df2["income"] * 1.1)
 df2["country"] = df2["country"].replace("USA","US")
 df2.columns = ["Id","Name","Age","Country","Gender","Income","Tax","New_income"]
 df2.rename(columns = {"income":"salary"})
-print(df2)
+df2.rename(index = {1:"one"})
