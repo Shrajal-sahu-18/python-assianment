e0d06142fe2fcf0fda58e1cbde0deafbb3ddad9b
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-30 22:35:04 +0530

    data cleaning

--- Pandas/mini_project.py
+++ Pandas/mini_project.py
@@ -1,10 +1,13 @@
+#====================
+#import library
 import pandas as pd
 import kagglehub
 import os
 
 path = kagglehub.dataset_download("noopurbhatt/retail-intelligence-100k-user-behavior-dataset")
 print(os.listdir(path))
-
+#=====================
+#Load file
 df = pd.read_csv(path +"/retail_user_behavior_100k.csv")
 
 #=====================
@@ -16,3 +19,8 @@ print("\n shape:",df.shape)
 print("\n columns:", df.columns)
 print("\n Info:",df.info())
 print("\n First Five Rows:",df.head(5))
+
+#data cleaning
+#=====================
+#mising value
+print("\n missing value\n", df.isnull().sum())
