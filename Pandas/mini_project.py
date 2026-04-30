d0513a0a566f4fe6741c0c17f7f4f1e4e256c6f9
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-04-30 22:32:26 +0530

    Basic info

--- Pandas/mini_project.py
+++ Pandas/mini_project.py
@@ -14,3 +14,5 @@ df = pd.read_csv(path +"/retail_user_behavior_100k.csv")
 #Task - 1  Basic Info 
 print("\n shape:",df.shape)
 print("\n columns:", df.columns)
+print("\n Info:",df.info())
+print("\n First Five Rows:",df.head(5))
